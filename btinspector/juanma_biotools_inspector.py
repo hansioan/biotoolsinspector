f8b76431617d155ea8f3b3948b3e9060502b543a
Juan Manuel Medina Méndez <lmt243@hestia.binf.ku.dk>
2017-07-06 23:06:17 +0200

    Minor change, code implemented as function. Currently testing implementation with pandas

--- btinspector/juanma_biotools_inspector.py
+++ btinspector/juanma_biotools_inspector.py
@@ -1,22 +1,22 @@
 import csv
 
-# This script reads the original problem file as a .tsv ("curation.tsv") and writes a new .csv file ("corrections.csv")
-# with a column of descriptions, capitalizing the initial letters of those which are not originally capitalized and
-# adding an ending dot to those descriptions which do not have one. This column will be manually pasted to the
-# original .tsv file in the "fixed_tool" description column
+def biotools_inspector(input_tsv):
+    '''This function takes a .tsv file and writes its second column (descriptions) to a .csv output file,
+             capitalizing the initial letter of the descriptions and adding an ending dot'''
+    with open(input_tsv, "rb") as tsvin:
+        with open("/home/juanma/Downloads/corrections.csv", "wb") as csvout:
+            tsvin = csv.reader(tsvin, delimiter = "\t")
+            csvout = csv.writer(csvout)
 
-with open("/home/juanma/Downloads/curation.tsv", "rb") as tsvin:
-    with open("/home/juanma/Downloads/corrections.csv", "wb") as csvout:
-        tsvin = csv.reader(tsvin, delimiter = "\t")
-        csvout = csv.writer(csvout)
+            for row in tsvin:
+                description = row[1]
 
-        for row in tsvin:
-            description = row[1]
+                # Capitalization and ending dot criteria evaluation
+                if description[0].isupper() and description.endswith("."):
+                    csvout.writerow([description])
+                else:
+                    fixed_description = description[0].capitalize() + description[1:] + "."
+                    csvout.writerow([fixed_description])
 
-            # Capitalization and ending dot criteria evaluation
-            if description[0].isupper() and description.endswith("."):
-                csvout.writerow([description])
-            else:
-                fixed_description = description[0].capitalize() + description[1:] + "."
-                csvout.writerow([fixed_description])
 
+biotools_inspector(input_tsv = "/home/juanma/Downloads/curation.tsv")
