70f391c7e7639ce3e8e6cae6694e75808cdbca10
Juan Manuel Medina Méndez <lmt243@hestia.binf.ku.dk>
2017-07-16 18:25:07 +0200

    Function-separated capitalizer-dotter inspector + updated compilation of reporter functions

--- btinspector/juanma_biotools_inspector.py
+++ btinspector/juanma_biotools_inspector.py
@@ -1,4 +1,9 @@
+# coding: utf-8
+
 import csv
+import re
+
+# Compilation of reporter functions
 
 def dot_checker(text):
     ''' This function checks if the text ends with a dot '''
@@ -10,6 +15,64 @@ def capitalize_checker(text):
     if text[0].islower():
         return "Error: the description initial letter is not capitalized"
 
+def last_space_checker(text):
+    ''' This function checks if the second last character of the text is a space '''
+    if text[-2] == (" "):
+        return "Error: the description previous last character is a space"
+
+def beyond_dot_checker(text):
+    ''' This function checks if the text has any text after the ending dot '''
+    ending_regex = re.compile("\..+")
+    if ending_regex.search(text):
+        return "Error: the description has characters after the ending dot"
+
+def length_checker(text):
+    ''' This function checks if the length of the text is less than 10 or more than 500 characters '''
+    if len(text) < 10:
+        return "Error: the length of the description is too short"
+    elif len(text) > 500:
+        return "Error: the length of the description is too long"
+
+def url_checker(text):
+    ''' This function checks if the text contains an URL '''
+    url_regex = re.compile("http|www.")
+    if url_regex.search(text):
+        return "Error: the description contains an URL"
+
+def names_checker(text, name):
+    ''' This function checks if the text contains the tool name '''
+    name_regex = re.compile(name)
+    if name_regex.search(text):
+        return "Error: the description contains a tool name"
+
+def character_checker(text):
+    '''This function checks if the text contains some unwanted characters'''
+    new_line_regex = re.compile("\n")
+    carriage_return_regex = re.compile("↵")
+    tab_regex = re.compile("\t")
+    escape_regex = re.compile("\\bold")
+    bullet_point_regex = re.compile("•")
+    unknown_symbol_regex = re.compile("|�")
+
+    error_list = []
+
+    if new_line_regex.search(text):
+        error_list.append("Error: the description contains new lines")
+    if carriage_return_regex.search(text):
+        error_list.append("Error: the description contains carriage returns")
+    if tab_regex.search(text):
+        error_list.append("Error: the description contains tabs")
+    if escape_regex.search(text):
+        error_list.append("Error: the description contains escape characters")
+    if bullet_point_regex.search(text):
+        error_list.append("Error: the description contains bullet points")
+    if unknown_symbol_regex.search(text):
+        error_list.append("Error: the description contains unknown symbols")
+
+    return error_list
+
+
+# Compilation of corrector functions
 
 def dot_fixer(text):
     ''' This function returns a description with an ending dot if it does not have one '''
@@ -21,7 +84,7 @@ def capitalize_fixer(text):
     if text[0].islower():
         return text[0].capitalize() + text[1:]
 
-def dot_capitalized_fixer(text):
+def dot_capitalize_fixer(text):
     ''' This function returns an initial-capitalized and ending dot description if it is not '''
     if text[0].islower() and text[-1] != ".":
         return [text[0].capitalize() + text[1:] + "."]
@@ -43,7 +106,7 @@ def biotools_inspector(input_tsv):
 
                 # If the description is not initial-capitalized and it does not have an ending dot
                 if dot_checker(description) and capitalize_checker(description):
-                    tsvout.writerow(dot_capitalized_fixer(description))
+                    tsvout.writerow(dot_capitalize_fixer(description))
 
                 # If the description does not have an ending dot
                 elif dot_checker(description):
@@ -51,9 +114,8 @@ def biotools_inspector(input_tsv):
 
                 # If the description is not initial-capitalized
                 elif capitalize_checker(description):
-                    tsvout.writerow(capitalize_checker(description))
+                    tsvout.writerow(capitalize_fixer(description))
 
-                # For everything else, mastercard
                 else:
                     pass
 
