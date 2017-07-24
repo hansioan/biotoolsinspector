75c7ef3073c05495efed4a1a51a54064c74bc1f0
María Touceda Suárez <MeryTouceda@MacBook-Pro-de-Maria-2.local>
2017-07-24 12:38:21 +0200

    Relocation and fixing of code

--- btinspector/mery_biotools_inspector.py
+++ btinspector/mery_biotools_inspector.py
@@ -1,13 +1,13 @@
-
+import json
 
 def filter_jsonobj(json_file):
     """This function takes a single JSON object and returns a tuple
     of the name and description of the tool"""
-    import json
 
     file = open(json_file, "r")
     string = file.read()
     parsed_json = json.loads(string)
+    file.close()
 
     name = parsed_json["name"]
     description = parsed_json["description"]
