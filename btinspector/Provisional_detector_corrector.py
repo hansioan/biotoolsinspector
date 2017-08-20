fb5b6f5fd1149c7bb3e3f58b2bf4e638f8244d8b
Migle <survilaite.migle@gmail.com>
2017-08-20 23:20:08 +0200

    Proper and working command line tool

--- btinspector/Provisional_detector_corrector.py
+++ btinspector/Provisional_detector_corrector.py
@@ -1,17 +1,6 @@
 # coding: utf-8
-import sys
 import re
 import pandas as pd
-
-# reading in the data as a data frame
-# data = pd.read_table("problematic_descriptions_migle.tsv")
-
-
-# storing only the tool description column
-# descriptions = data.tool_description
-
-# storing only the tool name column
-# tool_names = data.tool_name
 
 
 def dot_checker(text):
@@ -172,6 +161,7 @@ def data_iterator(texts, names=None, dot_caps_checking=True, url_checking=True, 
     If url_checkomg is False, the text is not checked if it contains an URL
     If names_checking is False, the text is not checked if it contains a tool name
     If character_checking is False, the text is not checked if it contains unwanted characters
+    If character_fixing is False, unwanted characters are not removed from the text
     If to_file is False, the output is not written to the file and just returned as a list of tuples'''
     error_list = []
     # checking if the names are given to the function
@@ -216,16 +206,97 @@ def data_iterator(texts, names=None, dot_caps_checking=True, url_checking=True, 
             return error_df
 
 
-# print data_iterator(descriptions,tool_names, to_file=True, file_name="something")
+def __number2bool(number):
+    if number==1:
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
-    descriptions_file = open(sys.argv[1], "r")
-    names_file = open(sys.argv[2], "r")
-    descriptions = descriptions_file.readlines()
-    descriptions = [description.rstrip("\n") for description in descriptions]
-    descriptions = [description.decode("UTF-8") for description in descriptions]
-    names = names_file.readlines()
-    names =[name.rstrip("\n") for name in names]
-    names_file.close()
-    descriptions_file.close()
-    data_iterator(descriptions, names)
+    import argparse
+    parser = argparse.ArgumentParser(description = "Command line tool for checking tool descriptions")
+    parser.add_argument("-m", "--mode", help="Mode of the checker: \"full\" - checks descriptions with their names;" \
+                                            "\"parial\" - checks only the descriptions.")
+    parser.add_argument("-one-file", "--one", help="Input descriptions and names from one file. "
+                                                   "Usable only with mode \"full\".",
+                        required=False, action = 'store_true')
+    parser.add_argument("-d", "--descriptions", help="File with descriptions.")
+    parser.add_argument("-n", "--names", help="File with description names.")
+    parser.add_argument("-sentence", "--sentence_checker", help="Check if descriptions start with a capital letter and end"
+                                                                " with a dot. Options: \"1\" to use the argument,"
+                                                                " \"0\" for skipping the argument. Default is 1.",
+                        type=int, required=False, default=True)
+    parser.add_argument("-url", "--url_checker", help="Check if descriptions contain URLs. Options: \"1\" to use the argument, "
+                                                      "\"0\" for skipping the argument. Default is 1.",
+                        type=int, required=False, default=True)
+    parser.add_argument("-name", "--name_check", help="Check if the description contains names. Options: \"1\" to use the argument, "
+                                                      "\"0\" for skipping the argument. Default is 1.",
+                        type=int, required=False, default=True)
+    parser.add_argument("-length", "--length_check", help="Check if the description length is within the limits and "
+                                                          "the descriptions is made of more than one word. "
+                                                          "Options: \"1\" to use the argument, \"0\" for skipping the argument. "
+                                                          "Default is 1.",
+                        type=int, required=False, default=True)
+    parser.add_argument("-character", "--character_check", help="Check if the description contains unwanted characters "
+                                                                "Options: \"1\" to use the argument, \"0\" for skipping "
+                                                                "the argument. Default is 1.",
+                        type=int, required=False, default=True)
+    parser.add_argument("-char-fix", "--character_fix", help="Check if the description contains unwanted characters and "
+                                                             "remove them. Options: \"1\" to use the argument, \"0\" for "
+                                                             "skipping the argument. Default is 1.",
+                        type=int, required=False, default=True)
+    parser.add_argument("-to-file", "--file", help="Write all the output to a .csv file. Options: \"1\" to use the argument, "
+                                                   "\"0\" for skipping the argument. Default is 1.",
+                        type=int, required=False, default=True)
+    parser.add_argument("-filename", "--file_name", help="Choose the output file name if the argument -to-file is True. "
+                                                         "Default is \"Warnings\".",
+                        type=str, required=False, default="Warnings")
+
+    args = vars(parser.parse_args())
+
+    if args["mode"] == "full":
+        if args["one"]:
+            descriptions_names = pd.read_table(args["descriptions"], header=None, index_col=None)
+            names = descriptions_names[0]
+            descriptions = descriptions_names[1]
+
+            print data_iterator(descriptions, names,
+                                dot_caps_checking=__number2bool(args["sentence_checker"]),
+                                url_checking=__number2bool(args["url_checker"]),
+                                names_checking=__number2bool(args["name_check"]),
+                                length_checking=__number2bool(args["length_check"]),
+                                character_checking=__number2bool(args["character_check"]),
+                                character_fixing=__number2bool(args["character_fix"]),
+                                to_file=__number2bool(args["file"]),
+                                file_name=args["file_name"])
+        else:
+            descriptions = pd.read_table(args["descriptions"], header=None, index_col=None)
+            names = pd.read_table(args["names"], header=None, index_col=None)
+            names = names[0]
+            descriptions = descriptions[0]
+            print data_iterator(descriptions, names,
+                          dot_caps_checking=__number2bool(args["sentence_checker"]),
+                          url_checking=__number2bool(args["url_checker"]),
+                          names_checking=__number2bool(args["name_check"]),
+                          length_checking=__number2bool(args["length_check"]),
+                          character_checking=__number2bool(args["character_check"]),
+                          character_fixing=__number2bool(args["character_fix"]),
+                          to_file=__number2bool(args["file"]),
+                          file_name=args["file_name"])
+    elif args["mode"] == "partial":
+        descriptions = pd.read_table(args["descriptions"], header=None, index_col=None)
+        descriptions = descriptions[0]
+        print data_iterator(descriptions,
+                      dot_caps_checking=__number2bool(args["sentence_checker"]),
+                      url_checking=__number2bool(args["url_checker"]),
+                      names_checking=__number2bool(args["name_check"]),
+                      length_checking=__number2bool(args["length_check"]),
+                      character_checking=__number2bool(args["character_check"]),
+                      character_fixing=__number2bool(args["character_fix"]),
+                      to_file=__number2bool(args["file"]),
+                      file_name=args["file_name"])
+    else:
+        print "Mode Error: The mode is wrong." \
+              "\nUsage: python btcorrector.py <mode> <description file> <name file>" \
+              "\n<mode>: \"full\" or \"partial\""
+
